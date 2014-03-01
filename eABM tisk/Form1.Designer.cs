a9ee132ef61aec062dac0c347e5b35c4c44133db
Desttro <petr@sabrnak.cz>
2014-03-01 11:41:41 +0100

    první commit

---
+++ eABM tisk/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace eABM_tisk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.checkAlwaysTop = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkAlwaysTop
+            // 
+            this.checkAlwaysTop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkAlwaysTop.AutoSize = true;
+            this.checkAlwaysTop.Checked = true;
+            this.checkAlwaysTop.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkAlwaysTop.Location = new System.Drawing.Point(10, 91);
+            this.checkAlwaysTop.Name = "checkAlwaysTop";
+            this.checkAlwaysTop.Size = new System.Drawing.Size(85, 17);
+            this.checkAlwaysTop.TabIndex = 2;
+            this.checkAlwaysTop.Text = "vždy nahoře";
+            this.checkAlwaysTop.UseVisualStyleBackColor = true;
+            this.checkAlwaysTop.CheckedChanged += new System.EventHandler(this.checkAlwaysTop_CheckedChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 25);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 56);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Vyberte tiskárnu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(147, 120);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.checkAlwaysTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "eABM tisk";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkAlwaysTop;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ eABM tisk/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Drawing.Printing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace eABM_tisk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            listAllPrinters();
+            this.TopMost = checkAlwaysTop.Checked;
+        }
+
+        private void listAllPrinters()
+        {
+            foreach (var item in PrinterSettings.InstalledPrinters)
+            {
+                this.listBox1.Items.Add(item.ToString());
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string pname = this.listBox1.SelectedItem.ToString();
+            myPrinters.SetDefaultPrinter(pname);
+        }
+
+        public static class myPrinters
+        {
+            [DllImport("winspool.drv", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern bool SetDefaultPrinter(string Name);
+        }
+
+        private void checkAlwaysTop_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = checkAlwaysTop.Checked;
+        }
+    }
+}
